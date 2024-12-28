21bb3e1dfb68ac5250e327c8bcbe33f24d3796d0
Jason Zhang <zjjc123@hotmail.com>
2024-12-28 14:10:45 -0800

    requirements and gitignore

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from basics import *
 
+
 def note(freq, dur):
-    return fade_in_lin(fade_out_lin(s_saww(freq,dur),.25),0.1)
+    return fade_in_lin(fade_out_lin(s_saww(freq, dur), .25), 0.1)
+
 
 a = note(900, .5)
 b = note(953.5, .5)
 rest = np.zeros(d2s(.5))
-x = np.concatenate((rest,a,a,a,a,a,a,rest,b,b,b,a,a,a,a,rest))
+x = np.concatenate((rest, a, a, a, a, a, a, rest, b, b, b, a, a, a, a, rest))
 write_wav(x)
 
-write_wav(fade_in_lin(fade_out_lin(s_white(10),4),4),'ocean')
+write_wav(fade_in_lin(fade_out_lin(s_white(10), 4), 4), 'ocean')
